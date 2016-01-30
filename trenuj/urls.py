ff73bbe955dc5c08ad7a8a132f00de93f6962d6b
Akus93 <dawid.rdzanek@gmail.com>
2016-01-30 18:37:40 +0100

    Zmiana czasu i jezyka

--- trenuj/urls.py
+++ trenuj/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-       url(r'^home/', views.IndexView.as_view(), name='index'),
+       url(r'^$', views.IndexView.as_view(), name='index'),
+       url(r'^signup/$', views.SignupView.as_view(), name='signup'),
 ]
--- trenuj/views.py
+++ trenuj/views.py
@@ -1,5 +1,25 @@
 from django.views import generic
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+from .forms import *
 
 
 class IndexView(generic.TemplateView):
     template_name = 'trenuj/index.html'
+
+
+class SignupView(generic.TemplateView):
+    form_class = SignupForm
+    template_name = 'trenuj/signup.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+        return render(request, self.template_name, {'form': form})
