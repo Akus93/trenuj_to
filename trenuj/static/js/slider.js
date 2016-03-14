2916ef8046139321fbe4d3f9ccea18a721c34a93
remi <remi@nativetap.io>
2016-03-14 22:01:03 +0100

    duży slider

--- trenuj/static/js/slider.js
+++ trenuj/static/js/slider.js
@@ -891,3 +891,42 @@ $.fn[plugin].fx = {};
  * OF THE POSSIBILITY OF SUCH DAMAGE. 
  *
  */
+
+        $('.link').click(function(){
+            var ind=3;
+            ind--;
+            $('#slides').superslides('animate' , ind)
+         });
+
+        document.onclick = function() {
+            if($('#register').css('display') == 'block')
+            {
+                $('.slides-navigation').css("display","none");
+            }
+            else
+            {
+                $('.slides-navigation').css("display","block");
+            }
+        };
+
+        $(document).on('keyup', function() {
+                if($('#register').css('display') == 'block')
+                {
+                    $('.slides-navigation').css("display","none");
+                }
+                else
+                {
+                    $('.slides-navigation').css("display","block");
+                }
+        });
+
+        setInterval(function() {
+                if($('#register').css('display') == 'block')
+                {
+                    $('.slides-navigation').css("display","none");
+                }
+                else
+                {
+                    $('.slides-navigation').css("display","block");
+                }
+        },0.1);
