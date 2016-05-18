0400ee3d72f32228a590768d2f7bc716b800b615
Akus93 <dawid.rdzanek@protonmail.com>
2016-05-18 21:39:02 +0200

    okienka wycinania zdjec

---
+++ trenuj/static/js/shortcut-img.js
@@ -0,0 +1,104 @@
+var AddShortcutImage = (function() {
+
+	function output(node) {
+		var existing = $('#result .croppie-result');
+		if (existing.length > 0) {
+			existing[0].parentNode.replaceChild(node, existing[0]);
+		}
+		else {
+			$('#result')[0].appendChild(node);
+		}
+	}
+
+	function shortcutImgUpload() {
+		var $uploadCrop;
+
+		function readFile(input) {
+ 			if (input.files && input.files[0]) {
+	            var reader = new FileReader();
+
+	            reader.onload = function (e) {
+	            	$uploadCrop.croppie('bind', {
+	            		url: e.target.result
+	            	});
+	            	$('.upload-contener').addClass('ready');
+	            };
+
+	            reader.readAsDataURL(input.files[0]);
+	        }
+	        else {
+		        swal("Sorry - you're browser doesn't support the FileReader API");
+		    }
+		}
+
+		$uploadCrop = $('#upload-contener').croppie({
+			viewport: {
+				width: 350,
+				height: 350,
+				type: 'square'
+			},
+			boundary: {
+				width: 450,
+				height: 450
+			},
+			exif: false
+		});
+
+		$('#choose_image').on('change', function () { readFile(this); });
+		$('.upload-result').on('click', function (ev) {
+			$uploadCrop.croppie('result', {
+				type: 'canvas',
+				size: 'viewport'
+			}).then(function (resp) {
+                $('#imagebase64').val(resp);
+                $('#link-create-form').submit();
+			});
+            return false;
+		});
+	}
+
+
+
+	function init() {
+		shortcutImgUpload();
+	}
+	return {
+		init: init
+	};
+})();
+
+
+// Full version of `log` that:
+//  * Prevents errors on console methods when no console present.
+//  * Exposes a global 'log' function that preserves line numbering and formatting.
+(function () {
+  var method;
+  var noop = function () { };
+  var methods = [
+      'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+      'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+      'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+      'timeStamp', 'trace', 'warn'
+  ];
+  var length = methods.length;
+  var console = (window.console = window.console || {});
+
+  while (length--) {
+    method = methods[length];
+
+    // Only stub undefined methods.
+    if (!console[method]) {
+        console[method] = noop;
+    }
+  }
+
+
+  if (Function.prototype.bind) {
+    window.log = Function.prototype.bind.call(console.log, console);
+  }
+  else {
+    window.log = function() {
+      Function.prototype.apply.call(console.log, console, arguments);
+    };
+  }
+})();
--- trenuj/views.py
+++ trenuj/views.py
@@ -57,9 +57,8 @@ class LinkCreateView(LoginRequiredMixin, generic.View):
         return render(request, self.template_name, {'form': form})
 
     def post(self, request, *args, **kwargs):
-        if request.FILES.get('image'):
-            extension = request.FILES['image'].name.split('.')[-1]
-            request.FILES['image'].name = '{0}.{1}'.format(uuid4(), extension)
+        img64 = request.POST['imagebase64'].split(',')[1]
+        request.FILES['image'] = ContentFile(b64decode(img64), '{}.png'.format(uuid4()))
         tags = request.POST.get('tags', '')
         form = self.form_class(request.POST, request.FILES, author=request.user.id)
         if form.is_valid():
@@ -80,9 +79,8 @@ class ImageCreateView(LoginRequiredMixin, generic.View):
         return render(request, self.template_name, {'form': form})
 
     def post(self, request, *args, **kwargs):
-        if request.FILES.get('image'):
-            extension = request.FILES['image'].name.split('.')[-1]
-            request.FILES['image'].name = '{0}.{1}'.format(uuid4(), extension)
+        img64 = request.POST['imagebase64'].split(',')[1]
+        request.FILES['image'] = ContentFile(b64decode(img64), '{}.png'.format(uuid4()))
         tags = request.POST.get('tags', '')
         form = self.form_class(request.POST, request.FILES, author=request.user.id)
         if form.is_valid():
